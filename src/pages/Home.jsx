851159ba370e9537b13a973f9328f047b21f12fa
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-12-23 11:52:12 -0300

    Coloca hover nos botoes do hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,10 @@ export default function Home() {
               Desenvolvo aplicações FullStack.
             </h1>
             <div className='ml-4 mt-4'>
-              <button className='bg-grey px-8 py-4 mx-5 my-4 text-white text-2xl rounded-full'>
+              <button className='bg-grey px-8 py-4 mx-5 my-4 text-white text-2xl rounded-full transition-all hover:px-12'>
                 Sobre mim
               </button>
-              <button className='bg-purple px-10 py-4 mx-5 my-4 text-white text-2xl rounded-full'>
+              <button className='bg-purple px-10 py-4 mx-5 my-4 text-white text-2xl rounded-full transition-all hover:px-12'>
                 Meus Projetos
               </button>
             </div>
@@ -60,7 +60,8 @@ export default function Home() {
             projectImg={imgTrunfoProject}
             reversed
           >
-            Preencha um formulário para criar um baralho do famoso jogo dos anos 90 "Super Trunfo".
+            Preencha um formulário para criar um baralho do famoso jogo dos anos
+            90 "Super Trunfo".
           </ProjectCard>
         </div>
       </section>
