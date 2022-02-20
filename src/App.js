e96fc4d03f5f518603b5ad24c60a1ba896ce6df0
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-02-19 23:12:58 -0300

    Reset Portifolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './components/Header'
-import Home from './pages/Home'
 
 export default function App() {
   return (
     <div className="bg-black h-screen">
       <Header />
-      <Home />
     </div>
   )
 }
